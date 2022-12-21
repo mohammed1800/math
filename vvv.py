3e155256fd1eb7297f5d646e94a630d5f1f3b4ec
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2022-12-21 10:09:19 +0530

    Add files via upload

---
+++ vvv.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Transpose a row vector
+vector = np.array([1, 2, 3])
+transposed_vector = vector.T
+
+# Transpose a column vector
+vector = np.array([[1], [2], [3]])
+transposed_vector = vector.T
+
+# Transpose a matrix
+matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+transposed_matrix = matrix.T
