0d35a81245710b1c2d6f7579f13d3d1d8c22e037
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2023-02-21 12:20:49 +0530

    Create practical 11.py

---
+++ practical 11.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def gradient(f, x, h=1e-4):
+    gradient = np.zeros_like(x)
+    for i in range(x.shape[0]):
+        x_plus = np.copy(x)
+        x_minus = np.copy(x)
+        x_plus[i] += h
+        x_minus[i] -= h
+        gradient[i] = (f(x_plus) - f(x_minus)) / (2 * h)
+    return gradient
+
+# Example usage
+def f(x):
+    return x[0]**2 + x[1]**2
+
+x = np.array([1, 2])
+grad = gradient(f, x)
+print("Gradient: ", grad)
