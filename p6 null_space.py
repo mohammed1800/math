534afa50138e309ead6d5dfea09794d33da8255b
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2024-12-29 02:40:02 +0530

    Update and rename practical 6.py to p6 null_space.py

--- p6 null_space.py
+++ p6 null_space.py
@@ -2,16 +2,13 @@ import numpy as np
 from scipy.linalg import null_space, orth
 
 def basis_of_matrix_space(matrix):
-    # Compute the basis of column space
+ 
     col_basis = orth(matrix.T)
 
-    # Compute the basis of null space
     null_basis = null_space(matrix)
 
-    # Compute the basis of row space
     row_basis = orth(matrix)
 
-    # Compute the basis of left null space
     left_null_basis = null_space(matrix.T)
 
     return col_basis, null_basis, row_basis, left_null_basis
