c5d1348f8694b1be2a8a1d8fc2cc1e864d406598
mohammed1800 <119121182+mohammed1800@users.noreply.github.com>
2024-12-29 02:22:50 +0530

    Update the transpose vector (matrix) 1.py

--- practical 1.py
+++ practical 1.py
@@ -1,13 +1,19 @@
 import numpy as np
 
-# Transpose a row vector
-vector = np.array([1, 2, 3])
-transposed_vector = vector.T
+v = np.array([1, 2, 3]) 
+v_transpose = v.reshape(-1, 1)  
 
-# Transpose a column vector
-vector = np.array([[1], [2], [3]])
-transposed_vector = vector.T
+A = np.array([[1, 2], [3, 4]])
 
-# Transpose a matrix
-matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-transposed_matrix = matrix.T
+A_transpose = A.T
+
+B = np.array([[1 + 1j, 2], [3, 4 - 1j]])
+
+B_conjugate_transpose = B.conj().T
+
+print("Vector (row):", v)
+print("Vector (column):\n", v_transpose)
+print("Matrix A:\n", A)
+print("Transpose of A:\n", A_transpose)
+print("Complex Matrix B:\n", B)
+print("Conjugate Transpose of B:\n", B_conjugate_transpose)
